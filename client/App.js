3532928682cf93614fe2b5ea9bd37f6c0c669c84
Rudy <goulleyrudy@gmail.com>
2023-06-21 17:00:29 +0200

    first push rudy

--- client/App.js
+++ client/App.js
@@ -1,7 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
 import { Text, View, Button, Platform } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
+import IdentificationScreen from './screens/IdentificationScreen';
+import ConnexionScreen from './screens/ConnexionScreen';
+import InscriptionScreen from './screens/InscriptionScreen';
+import AcceuilScreen from './screens/AcceuilScreen';
+import InscriptionScreen from './screens/NotificationsScreen';
+import AddNotificationScreen from './screens/AddNotificationScreen';
+import SetNotificationsScreen from './screens/SetNotificationsScreen';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -35,28 +44,22 @@ export default function App() {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title="Press to schedule a notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Identification" component={IdentificationScreen} />
+        <Stack.Screen name="Connexion" component={ConnexionScreen} />
+        <Stack.Screen name="Inscription" component={InscriptionScreen} />
+        <Stack.Screen name="Accueil" component={AcceuilScreen} />
+        <Stack.Screen name="Notification détails" component={NotificationsScreen} />
+        <Stack.Screen name="Ajout de Notification" component={AddNotificationScreen} />
+        <Stack.Screen name="Modification de Notifications" component={SetNotificationsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+
+//FONCTION INUTILE POUR LE MOMENT, SERVAIENT POUR LE TEST DE BONNE RECEPTION DE NOTIFICATION : FONCTIONNEL
 async function schedulePushNotification() {
   await Notifications.scheduleNotificationAsync({
     content: {
@@ -68,34 +71,4 @@ async function schedulePushNotification() {
   });
 }
 
-async function registerForPushNotificationsAsync() {
-  let token;
 
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  return token;
-}
---
+++ client/screens/AcceuilScreen.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native';
+import axios from 'axios';
+
+
+export default function AcceuilScreen({navigation}) {
+
+}
