c5eab0b02d2ad5ccaea4e5878f9a92681ec13fda
killiancolla <killiancolla@hotmail.fr>
2023-06-21 17:21:31 +0200

    ajout d'inscription / connexion api

--- client/App.js
+++ client/App.js
@@ -1,10 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
+import MapView, { Marker } from 'react-native-maps';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const lat = 48.848242
+  const long = 2.395661
+  const rayon = 50
+
+  const [location, setLocation] = useState(null)
+  const [onZone, setOnZone] = useState(false)
+
+  function getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
+    const R = 6371e3; // Rayon moyen de la terre en mètres
+    const dLat = deg2rad(lat2 - lat1);  // Convertir les degrés en radians
+    const dLon = deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distance en mètres
+    return distance;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+  }
+
+  useEffect(() => {
+    getLocationAsync()
+  }, [])
+
+  useEffect(() => {
+    if (location !== null) {
+      const distance = getDistanceFromLatLonInMeters(location.latitude, location.longitude, lat, long);
+      if (distance <= rayon) {
+        setOnZone(true)
+      } else {
+        setOnZone(false)
+      }
+    }
+
+  }, [location])
+
+  const getLocationAsync = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync()
+    if (status === 'granted') {
+      const currentLocation = await Location.getCurrentPositionAsync({})
+      setLocation(currentLocation.coords)
+    } else {
+      console.log('Permissions de localisation refusées');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {onZone && (
+        <>
+          <Text>Vous êtes dans la zone</Text>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
---
+++ server/routes/users.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const Users = require('../models/Users');
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        // Recherchez l'utilisateur dans la base de données
+        const user = await Users.findOne({ email: username }); // Trouvez l'utilisateur par son nom d'utilisateur
+        if (!user) {
+            return done(null, false, { message: 'Incorrect email.' });
+        }
+
+        // Vérifiez le mot de passe
+        const isMatch = await bcrypt.compare(password, user.motDePasse);
+        if (!isMatch) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}));
+
+router.get('/', async (req, res) => {
+    const users = await Users.find();
+    res.json(users);
+});
+
+router.post('/register', async (req, res) => {
+    try {
+        const { email, motDePasse } = req.body;
+
+        // Vérifiez si l'utilisateur existe déjà
+        const existingUser = await Users.findOne({ login: email }); // Trouvez l'utilisateur par son nom d'utilisateur
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already exists.' });
+        }
+
+        // // Hachez le mot de passe
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(motDePasse, salt);
+
+        // // Créez un nouvel utilisateur
+        const newUser = new Users({
+            email: email,
+            motDePasse: hashedPassword,
+        });
+
+        // // Sauvegardez l'utilisateur dans la base de données
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully.' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error registering user.', error: err });
+    }
+});
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+        if (err || !user) {
+            return res.status(400).json({
+                message: 'Something is not right',
+                user: user,
+            });
+        }
+
+        req.login(user, { session: false }, (err) => {
+            if (err) {
+                res.send(err);
+            }
+
+            const token = jwt.sign({ id: user.id }, 'your_jwt_secret', {
+                expiresIn: '1h',
+            });
+
+            res.status(201).json({ id: user.id });
+        });
+    })(req, res, next);
+});
+
+module.exports = router;
