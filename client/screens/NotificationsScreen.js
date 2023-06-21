cd8ff030a1797333a66cff8cc43da1bd0766cce5
Adrien Delacroix <57764513+Delacroix-75020@users.noreply.github.com>
2023-06-21 17:25:58 +0200

    Ajout Affichage des notification

---
+++ client/screens/NotificationsScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import axios from 'axios';
+
+const NotificationsScreen = () => {
+  const [lieuxNotifications, setLieuxNotifications] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://your-api-url.com/notifications') // Remplacez par votre URL d'API
+      .then((response) => {
+        setLieuxNotifications(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={{ padding: 10, borderBottomColor: '#ccc', borderBottomWidth: 1 }}>
+      <Text style={{ fontSize: 18 }}>{item.nom}</Text>
+      <Text>Latitude: {item.latitude}, Longitude: {item.longitude}</Text>
+      <Text>Rayon: {item.rayon} mètres</Text>
+      <Text>Message: {item.message}</Text>
+    </View>
+  );
+
+  return (
+    <FlatList
+      data={lieuxNotifications}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+    />
+  );
+};
+
+export default NotificationsScreen;
