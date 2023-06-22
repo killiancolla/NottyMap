c348b885a1c40cbb437de73fbd26181702e5a647
Adrien Delacroix <57764513+Delacroix-75020@users.noreply.github.com>
2023-06-22 09:46:12 +0200

    commit

--- client/screens/AccueilScreen.js
+++ client/screens/AccueilScreen.js
@@ -1,6 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import geolib from 'geolib';
+import PushNotification from "react-native-push-notification";
 import axios from 'axios';
 
+const AccueilScreen = () => {
+  const [userLocation, setUserLocation] = useState(null);
+  const [lieuxNotifications, setLieuxNotifications] = useState([]);
 
-export default function AccueilScreen({navigation}) {
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ latitude, longitude });
+      },
+      (error) => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }, []);
+
+  useEffect(() => {
+    axios.get('http://your-api-url.com/notifications')
+      .then((response) => {
+        setLieuxNotifications(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (userLocation) {
+      lieuxNotifications.forEach((lieu) => {
+        const distance = geolib.getDistance(
+          { latitude: userLocation.latitude, longitude: userLocation.longitude },
+          { latitude: lieu.latitude, longitude: lieu.longitude }
+        );
+
+        if (distance < lieu.rayon) {
+          PushNotification.localNotification({
+            message: lieu.message,
+          });
+        }
+      });
+    }
+  }, [userLocation, lieuxNotifications]);
+
+  return (
+    <View style={{ padding: 20 }}>
+      {userLocation ? (
+        <>
+          <Text style={{ fontSize: 18 }}>Votre localisation:</Text>
+          <Text>Latitude: {userLocation.latitude}</Text>
+          <Text>Longitude: {userLocation.longitude}</Text>
+        </>
+      ) : (
+        <Text>Récupération de votre localisation...</Text>
+      )}
+    </View>
+  );
+};
+
+export default AccueilScreen;
