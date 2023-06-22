c30607765fea343f057ddbb84faa7dfd907444ec
Rudy <goulleyrudy@gmail.com>
2023-06-22 11:25:41 +0200

    add inscriptionscreen

---
+++ client/screens/InscriptionScreen.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+const InscriptionScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleInscription = async () => {
+    if (password !== confirmPassword) {
+      Alert.alert("Erreur", "Les mots de passe ne correspondent pas");
+      return;
+    }
+
+    // Ici, ajoutez le code pour gérer l'inscription
+    const token = await Notifications.getExpoPushTokenAsync({
+        experienceId: '@username/projectSlug', // Replace with your expo username and project slug
+        guid: 'YOUR_GUID', // Replace with your GUID
+        type: 'apns',
+        development: true,
+    })
+    .then(token => {
+        console.log(token.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+  };
+
+  return (
+    <KeyboardAvoidingView 
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Inscription</Text>
+
+          <TextInput
+            style={styles.input}
+            onChangeText={setName}
+            value={name}
+            placeholder="Nom"
+          />
+
+          <TextInput
+            style={styles.inputSmall}
+            onChangeText={setAge}
+            value={age}
+            placeholder="Age"
+            keyboardType="number-pad"
+          />
+
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Mot de passe"
+            secureTextEntry
+          />
+
+          <TextInput
+            style={styles.input}
+            onChangeText={setConfirmPassword}
+            value={confirmPassword}
+            placeholder="Confirmez le mot de passe"
+            secureTextEntry
+          />
+
+          <TouchableOpacity 
+            style={styles.inscriptionButton} 
+            onPress={handleInscription}
+          >
+            <Text style={styles.inscriptionText}>S'inscrire</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  inputSmall: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    width: 100,
+  },
+  inscriptionButton: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#2196f3',
+  },
+  inscriptionText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default InscriptionScreen;
